11eca8cfceaea37a09453059e7029c3ae80fc4d6
SamarthTumdi <samarth.tumdi@gmail.com>
2022-12-20 01:08:50 +0530

    CTP partial completion

--- columnar_tp.py
+++ columnar_tp.py
@@ -1,6 +1,13 @@
 
 #~ Python program to implement columnar transposition cipher
 
+def ctp_encryption(plain_text: str, key: str):
+  keylen = len(key)
+  ptlen = len(plain_text)
+  enc_mx = [[plain_text[i: i + keylen]] for i in range(0, ptlen - keylen + 1, keylen)]
+
+  print(enc_mx, sep="\n")
+
 def columnar_tp():
   plain_text = input("Enter the plain text (preferably ALL CAPS, no SPACE): ").upper().replace(" ", "")
   key = input("Enter the key (preferably shorter than the Plaintext or it will be sliced): ")[:len(plain_text)].upper().replace(" ", "")
@@ -10,6 +17,5 @@ def columnar_tp():
       plain_text += 'X'
     else:
       break
-  
-  enc_mx = []
+  ctp_encryption(plain_text, key)
 columnar_tp()
