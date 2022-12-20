b8ea962e1017deee976dc4e3b6acb5984fd9f6ad
SamarthTumdi <samarth.tumdi@gmail.com>
2022-12-20 13:27:23 +0530

    Implemented Columnar Transposition Cipher

--- columnar_tp.py
+++ columnar_tp.py
@@ -1,13 +1,12 @@
 
 #~ Python program to implement columnar transposition cipher
 
+# Encryption -> Encryption_mx => PLaintext filled row-wise; According to the alphabetic order of the key's chars. pick columns in enc_mx and append those chars. in cipher_text
 def ctp_encryption(plain_text: str, key: str):
-  keylen = len(key)
-  ptlen = len(plain_text)
+  keylen, ptlen = len(key), len(plain_text)
   cipher_text = ""
-  enc_mx = [[plain_text[i: i + keylen]] for i in range(0, ptlen - keylen + 1, keylen)]      #~ +1 because endpoint is excluded but I want it included (NTS: Maybe do it the DES way)
+  enc_mx = [list(plain_text[i: i + keylen]) for i in range(0, ptlen - keylen + 1, keylen)]      #~ +1 because endpoint is excluded but I want it included (NTS: Maybe do it the DES way)(Another NTS: Don't keep it as list of single strings, keep it as list of chars.)
 
-  # 'enumerating' the key's chars. for corresponding columns' chars. to be picked up for cipher text
   keylist = list(key)
   sorted_keylist = sorted(list(key))
   included_indx = []
@@ -17,14 +16,35 @@ def ctp_encryption(plain_text: str, key: str):
     for j in range(len(keylist)):
       if sorted_keylist[i] == keylist[j] and j not in included_indx:           # if characters are equal and the index of the char. in the o.g keylist is not already used, use it as the column index for the enc_mx
         for row in range(len(enc_mx)):
-          cipher_text += enc_mx[row][0][j]
+          cipher_text += enc_mx[row][j]
         included_indx.append(j)
         break
 
   return cipher_text
 
-def cpt_decryption(cipher_text: str, key: str):
+# Decryption -> fill the columns with slices of cipher_text based on the same ascending-arrangement of the key
+def ctp_decryption(cipher_text: str, key: str):
   plain_text = ""
+  keylen, ctlen = len(key), len(cipher_text)
+  dec_mx = [[0 for i in range(keylen)] for j in range(int(ctlen / keylen))]
+  ct_curr_indx = 0
+  
+  keylist, sorted_keylist = list(key), sorted(list(key))      # I've not imported this from the encryption block to keep the essence of messaging between 2 users, receiver will have to process the key on their own
+  included_indx = []
+
+  for i in range(len(sorted_keylist)):
+    for j in range(len(keylist)):
+      if sorted_keylist[i] == keylist[j] and j not in included_indx:           
+        for row in range(len(dec_mx)):
+          dec_mx[row][j] = cipher_text[ct_curr_indx]
+          ct_curr_indx += 1
+        included_indx.append(j)
+        break
+
+  for _ in range(len(dec_mx)):
+    plain_text += "".join(dec_mx.pop(0))
+
+  return plain_text
 
 def columnar_tp():
   print("Columnar Transposition")
@@ -33,10 +53,12 @@ def columnar_tp():
   keylen = len(key)
   while True:
     if len(plain_text) % keylen != 0:
-      plain_text += 'X'
+      plain_text += '_'
     else:
       break
   cipher_text = ctp_encryption(plain_text, key)
   print("Cipher Text: ", cipher_text)
+  plain_text = ctp_decryption(cipher_text, key)
+  print("Plain Text: ", plain_text)
 
 columnar_tp()
